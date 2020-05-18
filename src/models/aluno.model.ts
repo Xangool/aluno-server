d4136c50026858ede449ddd12c157ca5fe2bd1ef
Alexandre <alexandre@thau.tech>
2020-05-18 20:14:26 -0300

    Implementa model de aluno.

---
+++ src/models/aluno.model.ts
@@ -0,0 +1,50 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Aluno extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nome: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  cpf: string;
+
+  @property({
+    type: 'string',
+  })
+  matricula?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  dataNascimento: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Aluno>) {
+    super(data);
+  }
+}
+
+export interface AlunoRelations {
+  // describe navigational properties here
+}
+
+export type AlunoWithRelations = Aluno & AlunoRelations;
