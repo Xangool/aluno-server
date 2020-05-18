7633ffb3580ac4c695cb07e2c87f34e9d5985a2a
Alexandre <alexandre@thau.tech>
2020-05-18 20:18:23 -0300

    Implementa controller de aluno.

---
+++ src/controllers/aluno.controller.ts
@@ -0,0 +1,175 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getFilterSchemaFor,
+  getModelSchemaRef,
+  getWhereSchemaFor,
+  patch,
+  put,
+  del,
+  requestBody,
+} from '@loopback/rest';
+import {Aluno} from '../models';
+import {AlunoRepository} from '../repositories';
+
+export class AlunoController {
+  constructor(
+    @repository(AlunoRepository)
+    public alunoRepository : AlunoRepository,
+  ) {}
+
+  @post('/alunos', {
+    responses: {
+      '200': {
+        description: 'Aluno model instance',
+        content: {'application/json': {schema: getModelSchemaRef(Aluno)}},
+      },
+    },
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Aluno, {
+            title: 'NewAluno',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    aluno: Omit<Aluno, 'id'>,
+  ): Promise<Aluno> {
+    return this.alunoRepository.create(aluno);
+  }
+
+  @get('/alunos/count', {
+    responses: {
+      '200': {
+        description: 'Aluno model count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async count(
+    @param.where(Aluno) where?: Where<Aluno>,
+  ): Promise<Count> {
+    return this.alunoRepository.count(where);
+  }
+
+  @get('/alunos', {
+    responses: {
+      '200': {
+        description: 'Array of Aluno model instances',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: getModelSchemaRef(Aluno, {includeRelations: true}),
+            },
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Aluno) filter?: Filter<Aluno>,
+  ): Promise<Aluno[]> {
+    return this.alunoRepository.find(filter);
+  }
+
+  @patch('/alunos', {
+    responses: {
+      '200': {
+        description: 'Aluno PATCH success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Aluno, {partial: true}),
+        },
+      },
+    })
+    aluno: Aluno,
+    @param.where(Aluno) where?: Where<Aluno>,
+  ): Promise<Count> {
+    return this.alunoRepository.updateAll(aluno, where);
+  }
+
+  @get('/alunos/{id}', {
+    responses: {
+      '200': {
+        description: 'Aluno model instance',
+        content: {
+          'application/json': {
+            schema: getModelSchemaRef(Aluno, {includeRelations: true}),
+          },
+        },
+      },
+    },
+  })
+  async findById(
+    @param.path.string('id') id: string,
+    @param.filter(Aluno, {exclude: 'where'}) filter?: FilterExcludingWhere<Aluno>
+  ): Promise<Aluno> {
+    return this.alunoRepository.findById(id, filter);
+  }
+
+  @patch('/alunos/{id}', {
+    responses: {
+      '204': {
+        description: 'Aluno PATCH success',
+      },
+    },
+  })
+  async updateById(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Aluno, {partial: true}),
+        },
+      },
+    })
+    aluno: Aluno,
+  ): Promise<void> {
+    await this.alunoRepository.updateById(id, aluno);
+  }
+
+  @put('/alunos/{id}', {
+    responses: {
+      '204': {
+        description: 'Aluno PUT success',
+      },
+    },
+  })
+  async replaceById(
+    @param.path.string('id') id: string,
+    @requestBody() aluno: Aluno,
+  ): Promise<void> {
+    await this.alunoRepository.replaceById(id, aluno);
+  }
+
+  @del('/alunos/{id}', {
+    responses: {
+      '204': {
+        description: 'Aluno DELETE success',
+      },
+    },
+  })
+  async deleteById(@param.path.string('id') id: string): Promise<void> {
+    await this.alunoRepository.deleteById(id);
+  }
+}
